b33501f014f3b362929bf92d9d6d2a51efdc4627
Yonghyun.Lee <yonghyun.lee@coway.com>
2022-01-27 14:31:10 +0900

    feat(#6): eslint airbnb 추가
    - 기존에 린트 룰이 많이 없어 원하는 수정이 안되는 문제로 airbnb 린트 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,32 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
-  'plugins': [
+  plugins: [
     'react',
     '@typescript-eslint',
   ],
-  'rules': {
-    '@typescript-eslint/explicit-function-return-type': 'error',
+  rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/default-param-last': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
-  'ignorePatterns': ['craco.config.js']
+  ignorePatterns: ['craco.config.js'],
 };
