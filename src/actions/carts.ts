fd0589ec6aa2fc601d037d40f68724ed2f63d03e
Yonghyun.Lee <yonghyun.lee@coway.com>
2022-01-27 18:22:53 +0900

    feat(#6): 상품 수량이 0일 때에도 장바구니에 목록이 추가되는 문제 수정
    - insertCart 시 예외처리 추가

--- src/actions/carts.ts
+++ src/actions/carts.ts
@@ -6,7 +6,7 @@ export const INSERT_ITEM_IN_CART = 'INSERT_ITEM_IN_CART' as const;
 export const RESET_CART = 'RESET_CART' as const;
 export const UPDATE_QUANTITY_ITEM_IN_CART = 'UPDATE_QUANTITY_ITEM_IN_CART' as const;
 
-export function unSafeInsertCartItem(item: CartItem): CartAction {
+export function unSafeInsertCartItem(item?: CartItem): CartAction {
   return {
     type: INSERT_ITEM_IN_CART,
     payload: item,
@@ -16,11 +16,12 @@ export function unSafeInsertCartItem(item: CartItem): CartAction {
 export function insertCartItem(item: CartItem): CartAction {
   const targetProduct = store.getState().products.find((product) => product.id === item.id)!;
   const targetCartItem = store.getState().cart.find((cartItem) => cartItem.id === item.id);
+
+  if (targetProduct.quantity === 0) {
+    return unSafeInsertCartItem();
+  }
   if (targetProduct.quantity < item.quantity + (targetCartItem?.quantity ?? 0)) {
-    return unSafeInsertCartItem({
-      ...item,
-      quantity: 0,
-    });
+    return unSafeInsertCartItem();
   }
   return unSafeInsertCartItem(item);
 }
